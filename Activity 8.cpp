3621c587ae6e05f891dc86348aff850f019adb4a
rayueeh <146305223+rayueeh@users.noreply.github.com>
2023-09-27 19:51:07 -0700

    Add files via upload

---
+++ Activity 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+      
+    string myname;
+    
+    cout<<"Enter your Name: ";
+    getline(cin,myname);
+    
+    cout<<"The lenght of your name is: "<<myname.size();
+ 	
+    
+}
